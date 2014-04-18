86ff2215045b0f1f185b2b0261cf971b3060e784
Dzmitry_Naskou <Dzmitry_Naskou@epam.com>
2014-04-18 23:34:54 +0300

    Added dependency to spring-context

--- src/main/java/org/springframework/data/orient/repository/object/config/EnableOrientObjectRepositories.java
+++ src/main/java/org/springframework/data/orient/repository/object/config/EnableOrientObjectRepositories.java
@@ -7,13 +7,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.springframework.beans.factory.FactoryBean;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.orient.repository.OrientRepository;
 import org.springframework.data.orient.repository.object.support.OrientObjectRepositoryFactoryBean;
-import org.springframework.data.repository.query.QueryLookupStrategy;
 import org.springframework.data.repository.query.QueryLookupStrategy.Key;
 
 @Target(ElementType.TYPE)
@@ -23,66 +21,21 @@ import org.springframework.data.repository.query.QueryLookupStrategy.Key;
 @Import(OrientObjectRepositoryRegistrar.class)
 public @interface EnableOrientObjectRepositories {
     
-    /**
-     * Alias for the {@link #basePackages()} attribute. Allows for more concise annotation declarations e.g.:
-     * {@code @EnableOrientRepositories("org.my.pkg")} instead of {@code @EnableOrientRepositories(basePackages="org.my.pkg")}.
-     */
     String[] value() default {};
 
-    /**
-     * Base packages to scan for annotated components. {@link #value()} is an alias for (and mutually exclusive with) this
-     * attribute. Use {@link #basePackageClasses()} for a type-safe alternative to String-based package names.
-     */
     String[] basePackages() default {};
 
-    /**
-     * Type-safe alternative to {@link #basePackages()} for specifying the packages to scan for annotated components. The
-     * package of each class specified will be scanned. Consider creating a special no-op marker class or interface in
-     * each package that serves no purpose other than being referenced by this attribute.
-     */
     Class<?>[] basePackageClasses() default {};
 
-    /**
-     * Specifies which types are eligible for component scanning. Further narrows the set of candidate components from
-     * everything in {@link #basePackages()} to everything in the base packages that matches the given filter or filters.
-     */
     Filter[] includeFilters() default {@Filter(type = FilterType.ASSIGNABLE_TYPE, value = OrientRepository.class)};
 
-    /**
-     * Specifies which types are not eligible for component scanning.
-     */
     Filter[] excludeFilters() default {};
 
-    /**
-     * Returns the postfix to be used when looking up custom repository implementations. Defaults to {@literal Impl}. So
-     * for a repository named {@code PersonRepository} the corresponding implementation class will be looked up scanning
-     * for {@code PersonRepositoryImpl}.
-     * 
-     * @return
-     */
     String repositoryImplementationPostfix() default "Impl";
 
-    /**
-     * Configures the location of where to find the Spring Data named queries properties file. Will default to
-     * {@code META-INFO/jpa-named-queries.properties}.
-     * 
-     * @return
-     */
     String namedQueriesLocation() default "";
     
-    /**
-     * Returns the key of the {@link QueryLookupStrategy} to be used for lookup queries for query methods. Defaults to
-     * {@link Key#CREATE_IF_NOT_FOUND}.
-     * 
-     * @return
-     */
     Key queryLookupStrategy() default Key.CREATE_IF_NOT_FOUND;
 
-    /**
-     * Returns the {@link FactoryBean} class to be used for each repository instance. Defaults to
-     * {@link OrientObjectRepositoryFactoryBean}.
-     * 
-     * @return
-     */
     Class<?> repositoryFactoryBeanClass() default OrientObjectRepositoryFactoryBean.class;
 }
