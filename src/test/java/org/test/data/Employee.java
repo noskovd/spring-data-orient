563bae6726fa0fa477bdce75360ba51554d73816
Dzmitry_Naskou <noskov.d@gmail.com>
2014-06-19 00:31:45 +0300

    Added new entity type

---
+++ src/test/java/org/test/data/Employee.java
@@ -0,0 +1,14 @@
+package org.test.data;
+
+public class Employee extends Person {
+
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
