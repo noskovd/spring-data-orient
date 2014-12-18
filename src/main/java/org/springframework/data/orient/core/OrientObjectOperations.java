f31d976da9f19bee9339d309c6fdd9a05eab29f3
Jarosław Kaczmarski <jarek.pl@gmail.com>
2014-12-18 13:09:12 +0100

    Implement OrientOperations for Document (OrientDocumentTemplate).(not finished)

--- src/main/java/org/springframework/data/orient/core/OrientObjectOperations.java
+++ src/main/java/org/springframework/data/orient/core/OrientObjectOperations.java
@@ -1,6 +1,12 @@
 package org.springframework.data.orient.core;
 
+import com.orientechnologies.orient.core.db.ODatabase;
+import com.orientechnologies.orient.core.record.ORecordInternal;
+
 public interface OrientObjectOperations extends OrientOperations {
 
     <RET> RET detachAll(Object iPojo, boolean returnNonProxiedInstance);
+
+    ODatabase<Object> delete(ORecordInternal iRecord);
+
 }
